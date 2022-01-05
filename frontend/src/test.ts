01bd6a35028644a2851360a1fe6d48ee1221b32c
Marco <moltenma@gmail.com>
2022-01-05 11:00:17 +0100

    upgraded to Angular 13 and Spring Boot 2.6.2

--- frontend/src/test.ts
+++ frontend/src/test.ts
@@ -1,4 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
+/* eslint-disable */
 
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
